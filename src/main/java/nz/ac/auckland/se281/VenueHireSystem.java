c1105cb6f93d34b2badf2eb28be1321c1f19eeba
Amir Mahdian <amah664@aucklanduni.ac.nz>
2024-04-18 00:26:11 +1200

    Moved Venues class to its own seperate file outside of VenueHireSystem

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -7,39 +7,6 @@ import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
 
-  // created new class named venue
-  public class Venue {
-    private String venueName;
-    private String venueCode;
-    private String capacity;
-    private String hireFee;
-
-    // setting input to my private variables using 'this'
-    public Venue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
-      this.venueName = venueName;
-      this.venueCode = venueCode;
-      this.capacity = capacityInput;
-      this.hireFee = hireFeeInput;
-    }
-
-    // setting all my getters to be used inside print venues and create venues
-    public String getVenueName() {
-      return venueName;
-    }
-
-    public String getVenueCode() {
-      return venueCode;
-    }
-
-    public String getCapacity() {
-      return capacity;
-    }
-
-    public String getHireFee() {
-      return hireFee;
-    }
-  }
-
   private List<Venue> venues;
 
   public VenueHireSystem() {
